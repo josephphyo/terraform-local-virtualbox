7180aa369266ab03ec8f731f92eb95d0bc8b0b27
Phyo Min Htun <htun@neozo.de>
2024-08-11 12:14:33 +0700

    change image version

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "virtualbox_vm" "node" {
   name   = var.vm_name
-  image  = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  image  = var.image
   cpus   = var.cpu_counts
   memory = var.memory
   #user_data = file("${path.module}/user_data")
--- variables.tf
+++ variables.tf
@@ -2,6 +2,11 @@ variable "vm_name" {
   description = "Name of VM"
 }
 
+variable "image" {
+  description = "Image Path"
+  default = "https://app.vagrantup.com/ubuntu/boxes/focal64/versions/20240809.0.0/providers/virtualbox.box"
+}
+
 variable "cpu_counts" {
   description = "VM CPU Counts"
   default     = 1
